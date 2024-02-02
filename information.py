0f41c802a584919fe707577f15b92770088b367a
superintgr <155528797+superintgr@users.noreply.github.com>
2024-02-02 17:05:42 -0500

    Create information.py

---
+++ information.py
@@ -0,0 +1,12 @@
+# information
+
+I : "information"
+i : "copy"
+S : "substrate"
+x : "variable"
+X : "state"
+
+def computation(substrate, medium):
+    """
+    Defines a function `C[permutation](S)` on substrate `S` of at least two possible state `x` in `X`, whose transformation consists of applying permutation to each `x` in substrate `S` where `x` -> `permutation(x)`
+    
