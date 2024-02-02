90e9dde7b8136eb430c04754ab2fab6a485a36a7
superintgr <155528797+superintgr@users.noreply.github.com>
2024-02-02 17:16:12 -0500

    Update information.py
    Preliminary code outlined for computation substrate and computation as construction event.

--- information.py
+++ information.py
@@ -8,5 +8,20 @@ X : "state"
 
 def computation(substrate, medium):
     """
-    Defines a function `C[permutation](S)` on substrate `S` of at least two possible state `x` in `X`, whose transformation consists of applying permutation to each `x` in substrate `S` where `x` -> `permutation(x)`
-    
+    Defines a function `C(S)` on substrate `S` of at least two possible state `x` in `X`, whose transformation consists of applying permutation to each `x` in substrate `S` where `x` -> `permutation(x)`.
+
+    C(S) := union(for all x in S -- {x -> permutations(x)}).
+    .. permutation task must be possible in order to cause the commutation relation to be possible, although with side effects allowed if required.
+    """
+    output = []
+    for state in substrate:
+        p = permutations(state)
+        result = []
+        for x in p:
+            if x in medium(x):
+                result.append(x)
+        variable = union(result)
+        output.append(variable)
+    return output
+            
+                
