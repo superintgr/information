bfc7b771834d4221144617b32bbe370bcde96ad5
superintgr <155528797+superintgr@users.noreply.github.com>
2024-02-03 10:37:12 -0500

    Update computation.py
    I made some simplification.

--- computation.py
+++ computation.py
@@ -1,11 +1,23 @@
 # computation
 
-def computation(state, operator):
-  variables = None
-  for x in state:
-    variable = (x, operator(x))
-    if variables == None:
-      variables = variable
-    else:
-      variables += variable
-  
+def pi(input):
+  output = [0] * len(input)
+  for index in range(len(input)):
+    variable = input[index]
+    if variable not in output:
+        output[index] = variable
+  return output
+
+def union(array):
+  output = []
+  for index in array:
+    if index not in output:
+      output.append(index)
+  return output
+
+def compute(memory, operator):
+  output = []
+  for slot in memory:
+    input = operator(slot)
+    output += input
+  return union(output)
